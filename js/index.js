d6730093f5d10dbab41cec64989ffc1f94cbfe63
sam-starikov <sam_starikov@rambler.ru>
2023-08-18 10:23:53 +0400

    разработка мобильной версии

---
+++ js/index.js
@@ -0,0 +1,16 @@
+let menu = document.querySelector('.menu')
+let burger = document.querySelector('.header__burger')
+
+/* burger.onclick = () => {
+	menu.classList.toggle('active')
+	burger.classList.toggle('active')
+	document.body.classList.toggle('lock')
+}
+ */
+burger.addEventListener('click', function clickOnBurger() {
+	menu.classList.toggle('active')
+	burger.classList.toggle('active')
+	// document.body.classList.toggle('lock')
+})
+
+console.log(burger)
