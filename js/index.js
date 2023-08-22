f896a33d886486c82edcb93085d62ab327561eab
sam-starikov <sam_starikov@rambler.ru>
2023-08-22 21:18:45 +0400

    доделали header

--- js/index.js
+++ js/index.js
@@ -1,14 +1,21 @@
 let menu = document.querySelector('.menu')
 let burger = document.querySelector('.header__burger')
+let selectClient = document.querySelectorAll('.select-client__item')
 
-/* burger.onclick = () => {
-	menu.classList.toggle('active')
-	burger.classList.toggle('active')
-	document.body.classList.toggle('lock')
-}
- */
+/* BURGER */
+
 burger.addEventListener('click', function clickOnBurger() {
 	menu.classList.toggle('active')
 	burger.classList.toggle('active')
 	document.body.classList.toggle('lock')
+})
+
+/* SELECT_CLIENT */
+
+selectClient.forEach(item => {
+	item.addEventListener('click', () => {
+		selectClient.forEach(elem => elem.classList.remove('active'))
+		item.classList.add('active')
+	})
+	// setInterval(() => item.classList.toggle('active'), 2000)
 })
