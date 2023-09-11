6557dd03207f209d03c7c44d9b864f82d66d61c4
sam-starikov <sam_starikov@rambler.ru>
2023-09-12 02:03:56 +0400

    адаптив mobile ver.

--- js/index.js
+++ js/index.js
@@ -1,11 +1,9 @@
 const nav = document.querySelector('nav')
 
 window.addEventListener('resize', () => {
-	if (window.innerWidth <= 376) {
+	if (window.innerWidth <= 385) {
 		nav.classList.remove('container')
 	} else {
 		nav.classList.add('container')
 	}
 })
-
-console.log()
